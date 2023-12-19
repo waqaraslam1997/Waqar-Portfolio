50ab2b9a48ba0482b7fd97505a09d4c13e6f80c8
Waqar Aslam <waqar.king10@gmail.com>
2023-12-20 01:18:40 +0500

    Completed Contact Page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { MailIcon, PhoneCall, HomeIcon } from "lucide-react";
+
+// component form
+import Form from "@/components/Form";
+
 
 const contact = () => {
   return (
-    <div>Contact Me</div>
-  )
-}
+    <section>
+      <div className="container mx-auto">
+        {/* text & illustration */}
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
+          {/* text */}
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className=" w-8 h-[2px] bg-primary"></span>
+              Say Hello 👋
+            </div>
+            <h1 className="h1 max-w-md mb-8">Let's Work Together</h1>
+            <p className="subtitle max-w-[400px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+              ipsam itaque.
+            </p>
+          </div>
+          {/* illustration */}
+          <div className="hidden xl:flex bg-contact_image_light dark:bg-contact_image_dark bg-no-repeat bg-top w-full"></div>
+        </div>
+        {/* info text & form */}
+        <div className="grid xl:grid-cols-2 mb-12 xl:mb-24 text-base xl:text-lg">
+          {/* info text */}
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24">
+            {/* mail */}
+            <div className="flex items-center gap-x-8 ">
+              <MailIcon size={18} className="text-primary" />
+              <div>waqar.king10@outlook.com</div>
+            </div>
+            {/* phone */}
+            <div className="flex items-center gap-x-8 ">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+92 311 0255089</div>
+            </div>
+            {/* address */}
+            <div className="flex items-center gap-x-8 ">
+              <HomeIcon size={18} className="text-primary" />
+              <div>Aswan Town, Malir-15, Karachi.</div>
+            </div>
+          </div>
+          {/* form */}
+          <Form />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default contact
+export default contact;
---
+++ src/components/Form.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+import { User, MailIcon, ArrowRightIcon, MessagesSquare } from "lucide-react";
+
+const Form = () => {
+  return (
+    <form className="flex flex-col gap-y-4">
+      {/* input */}
+      <div className="relative flex items-center">
+        <Input type="name" id="name" placeholder="Name" />
+        <User className="absolute right-8" size={20} />
+      </div>
+      {/* Email */}
+      <div className="relative flex items-center">
+        <Input type="email" id="email" placeholder="Email" />
+        <MailIcon className="absolute right-8" size={20} />
+      </div>
+      {/* Message */}
+      <div className="relative flex items-center">
+        <Textarea id="name" placeholder="Your Message Here!" />
+        <MessagesSquare className="absolute top-4 right-8" size={20} />
+      </div>
+      <Button className="flex items-center gap-x-1 max-w-[160px]">
+        Let's Talk
+        <ArrowRightIcon size={20}/>
+        </Button>
+    </form>
+  );
+};
+
+export default Form;
